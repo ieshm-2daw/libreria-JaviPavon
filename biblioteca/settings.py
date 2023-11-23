38cbde4fece84021ea1a879c8244493c44ef00b8
javipavon <javierpavonborrero@gmail.com>
2023-11-23 19:20:26 +0100

    Migrations Hecho y funcionando

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-AUTH_USER_MODEL = "biblioteca.App.Usuario"
+AUTH_USER_MODEL = "biblioteca_App.Usuario"
 
 
 # Quick-start development settings - unsuitable for production
--- biblioteca_App/admin.py
+++ biblioteca_App/admin.py
@@ -1,8 +1,9 @@
 # biblioteca_app/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario, Autor, Editorial, Libro, Prestamo
 
-admin.site.register(Usuario)
+admin.site.register(Usuario, UserAdmin)
 admin.site.register(Autor)
 admin.site.register(Editorial)
 admin.site.register(Libro)
