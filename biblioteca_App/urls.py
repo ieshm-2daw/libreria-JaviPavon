21e1672d78f3daf3f9dca5d0133547e1ebaee6c1
javipavon <javierpavonborrero@gmail.com>
2023-11-29 00:53:09 +0100

    update

--- biblioteca_App/urls.py
+++ biblioteca_App/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ListBooks, DetailBook, DeleteBook, EditBook
+from .views import ListBooks, DetailBook, DeleteBook, EditBook, FormBooks
 
 urlpatterns = [
     path('', ListBooks.as_view(), name='list_books'),
     path('detail/<int:pk>', DetailBook.as_view(), name='detail_book'),
     path('delete/<int:pk>', DeleteBook.as_view(), name='delete_book'),
     path('edit/<int:pk>', EditBook.as_view(), name='edit_book'),
+    path('createbook', FormBooks.as_view(), name='create_book'),
 
 ]
--- biblioteca_App/views.py
+++ biblioteca_App/views.py
@@ -6,6 +6,7 @@ from django.views.generic import (
     DetailView,
     DeleteView,
     UpdateView,
+    CreateView,
 )
 class ListBooks(ListView):
     model = Libro
@@ -20,7 +21,11 @@ class DeleteBook(DeleteView):
 
 class EditBook(UpdateView):
     model = Libro
-    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad','portada']
+    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad']
     template_name_suffix = "_update_form"
     success_url = reverse_lazy("list_books")
-
+    
+class FormBooks(CreateView):
+    model = Libro
+    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad']
+    success_url = reverse_lazy("list_books")
