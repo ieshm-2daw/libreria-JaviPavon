a7a21f18abeb4a2c8a50266116183523050357e6
javipavon <xjpavbor068@ieshnosmachado.org>
2023-11-29 10:43:17 +0100

    update

--- biblioteca_App/views.py
+++ biblioteca_App/views.py
@@ -1,3 +1,7 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render
+from django.views import View
 from .models import Libro
 from django.urls import reverse_lazy
 
@@ -8,8 +12,22 @@ from django.views.generic import (
     UpdateView,
     CreateView,
 )
+
 class ListBooks(ListView):
     model = Libro
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["libros_disponibles"] = Libro.objects.filter(disponibilidad="disponible")
+        context["libros_prestados"] = Libro.objects.filter(disponibilidad="prestado")
+        context["libros_en_proceso"] = Libro.objects.filter(disponibilidad="en_proceso")
+        return context
+
+    # def get_queryset(self):
+    #     return Libro.objects.filter(disponibilidad="disponible")
+    
+    #queryset = Libro.objects.filter(disponibilidad="disponible")
 
 
 class DetailBook(DetailView):
@@ -21,11 +39,11 @@ class DeleteBook(DeleteView):
 
 class EditBook(UpdateView):
     model = Libro
-    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad']
+    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad', 'portada']
     template_name_suffix = "_update_form"
     success_url = reverse_lazy("list_books")
     
 class FormBooks(CreateView):
     model = Libro
-    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad']
+    fields = ['titulo', 'autores','editorial','fecha_publicacion', 'genero','isbn','resumen', 'disponibilidad', 'portada']
     success_url = reverse_lazy("list_books")
