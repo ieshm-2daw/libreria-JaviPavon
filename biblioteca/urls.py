f03ddfad2f97e0be3e185a7d5fb9f9b1a9d3f19e
javipavon <xjpavbor068@ieshnosmachado.org>
2023-11-29 09:12:34 +0100

    update  imagen

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("biblioteca_App.urls"))
 ]
+
+urlpatterns += stativ(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
